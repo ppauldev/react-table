4c002303245b2fe9e12daab4d27eaf5880110288
Phillip Paul <phillippaul@gmx.de>
2022-08-26 19:14:21 +0200

    Add base table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 const App = () => {
   return (
-    <>
-      <header className="text-3xl font-bold">
-        Hello world!
-      </header>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>A</td>
+          <td>B</td>
+          <td>C</td>
+        </tr>
+        <tr>
+          <td>D</td>
+          <td>E</td>
+          <td>F</td>
+        </tr>
+        <tr>
+          <td>G</td>
+          <td>H</td>
+          <td>I</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
