8ccc571944cda07466ee391fbbcbbd3ddd7477c4
Phillip Paul <phillippaul@gmx.de>
2022-08-29 15:54:18 +0200

    Extend project structure, connect app to table provider

---
+++ src/api/data.ts
@@ -0,0 +1,27 @@
+export type TColumnsData = string[];
+export type TRowData = {
+  "1": string;
+  "2": string;
+  "3": string;
+};
+export type TRowsData = TRowData[];
+
+const data: TRowsData = [
+  {
+    "1": "A",
+    "2": "B",
+    "3": "C"
+  },
+  {
+    "1": "D",
+    "2": "E",
+    "3": "F"
+  },
+  {
+    "1": "G",
+    "2": "H",
+    "3": "I"
+  },
+];
+
+export default data;
