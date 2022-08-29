90e1f34a936eb3b5c8a2e805ca4395ff1a4f2eef
Phillip Paul <phillippaul@gmx.de>
2022-08-29 15:33:20 +0200

    Add table component contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
 const App = () => {
   return (
     <table>
@@ -29,24 +31,99 @@ const App = () => {
   );
 };
 
+type TColumnsData = string[];
+type TRowData = {
+  "1": string;
+  "2": string;
+  "3": string;
+};
+type TRowsData = TRowData[];
+
+const ColumnContext = createContext<TColumnsData | undefined>(undefined);
+const RowContext = createContext<TRowsData | undefined>(undefined);
+
+const useColumnContext = () => {
+  const context = useContext(ColumnContext);
+
+  if (context === undefined) {
+    throw new Error("useColumnContext was used outside its provider");
+  }
+
+  return context;
+};
+
+const useRowContext = () => {
+  const context = useContext(RowContext);
+
+  if (context === undefined) {
+    throw new Error("useRowContext was used outside its provider");
+  }
+
+  return context;
+};
+
+const TableContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [columnData, setColumnData] = useState([]);
+  const [rowData, setRowData] = useState([]);
+
+  useEffect(() => {
+    //
+  }, []);
+
+  return (
+    <ColumnContext.Provider value={columnData}>
+      <RowContext.Provider value={rowData}>
+        {children}
+      </RowContext.Provider>
+    </ColumnContext.Provider>
+  );
+};
+
 const Table = () => {
-  //
+  return (
+    <TableContextProvider>
+      <TableHead />
+      <TableBody />
+    </TableContextProvider>
+  );
 };
 
 const TableHead = () => {
-  //
+  const columns = useColumnContext();
+
+  return (
+    <thead>
+      <tr>
+        {columns.map((column, i) => <th key={i}>{column}</th>)}
+      </tr>
+    </thead>
+  );
 };
 
 const TableBody = () => {
-  //
+  const rows = useRowContext();
+
+  return (
+    <tbody>
+      {rows.map((row, i) => <TableRow key={i} {...row} />)}
+    </tbody>
+  );
 };
 
-const TableRow = () => {
-  //
+const TableRow = (rowProps: TRowData) => {
+  return (
+    <tr>
+      {Object.values(rowProps).map((value, i) => <TableCell key={i} value={value} />)}
+    </tr>
+  );
 };
 
-const TableCell = () => {
-  //
+const TableCell = ({ value }: { value: string }) => {
+  return (
+    <td>
+      {value}
+    </td>
+  );
 };
 
 export default App;
