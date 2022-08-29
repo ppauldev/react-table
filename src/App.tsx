2be24905486ec350fdcdbe2a121d4c91e8288eaa
Phillip Paul <phillippaul@gmx.de>
2022-08-29 16:47:15 +0200

    Apply table styling tailwindui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { TRowData } from "./api/data";
 import { TableContextProvider, useColumnContext, useRowContext } from "./contexts/TableContext";
 
+// Style via TailwindUI: https://tailwindui.com/components/application-ui/lists/tables
+
 const App = () => {
   return (
-    <div className="app">
-      <Table />
+    <div className="app min-h-screen bg-gray-100 text-gray-900">
+      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+        <h1 className="text-xl">React Table Demo</h1>
+        <div className="mt-4">
+          <div className="mt-2 flex flex-col">
+            <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+              <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                  <Table />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
@@ -12,7 +27,7 @@ const App = () => {
 const Table = () => {
   return (
     <TableContextProvider>
-      <table>
+      <table className="min-w-full divide-y divide-gray-200">
         <TableHead />
         <TableBody />
       </table>
@@ -24,9 +39,9 @@ const TableHead = () => {
   const columns = useColumnContext();
 
   return (
-    <thead>
+    <thead className="bg-gray-50">
       <tr>
-        {columns.map((column, i) => <th key={i}>{column}</th>)}
+        {columns.map((column, i) => <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" key={i}>{column}</th>)}
       </tr>
     </thead>
   );
@@ -36,7 +51,7 @@ const TableBody = () => {
   const rows = useRowContext();
 
   return (
-    <tbody>
+    <tbody className="bg-white divide-y divide-gray-200">
       {rows.map((row, i) => <TableRow key={i} {...row} />)}
     </tbody>
   );
@@ -52,7 +67,7 @@ const TableRow = (rowProps: TRowData) => {
 
 const TableCell = ({ value }: { value: string }) => {
   return (
-    <td>
+    <td className="px-6 py-4 whitespace-nowrap">
       {value}
     </td>
   );
