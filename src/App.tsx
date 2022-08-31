5bd6e3748da7df94232c0a4fa095e7dba278629b
Phillip Paul <phillippaul@gmx.de>
2022-08-31 14:37:23 +0200

    Add page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { TRowData } from "./api/data";
-import { TableContextProvider, useColumnContext, useRowContext } from "./contexts/TableContext";
+import { TableContextProvider, useColumnContext, usePaginationContext, useRowContext } from "./contexts/TableContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 // Style via TailwindUI: https://tailwindui.com/components/application-ui/lists/tables
@@ -80,41 +80,45 @@ const TableCell = ({ value }: { value: string }) => {
 };
 
 const Pagination = () => {
+  const { page, pageSize, setPage, count } = usePaginationContext();
+
   return (
     <section id="pagination" className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
       <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
             Showing
-            <span className="font-medium">1</span>
+            <span className="font-medium"> {page === 1 ? page : page * pageSize} </span>
             to
-            <span className="font-medium">10</span>
+            <span className="font-medium"> {page === 1 ? pageSize : (page * pageSize) + pageSize} </span>
             of
-            <span className="font-medium">97</span>
+            <span className="font-medium"> {count} </span>
             results
           </p>
         </div>
         <div>
           <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-            <a href="#" className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+            <button className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <span className="sr-only">Previous</span>
               <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
               </svg>
-            </a>
-            <a href="#" aria-current="page" className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"> 1 </a>
-            <a href="#" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"> 2 </a>
-            <a href="#" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"> 3 </a>
-            <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700"> ... </span>
-            <a href="#" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"> 8 </a>
-            <a href="#" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"> 9 </a>
-            <a href="#" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"> 10 </a>
-            <a href="#" className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+            </button>
+            {[...Array((Math.ceil(count / pageSize) * pageSize) / pageSize)].map((_, i) => {
+              const activeProps = { "aria-current": "page", "className": "z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium" };
+              const passiveProps = { "className": "bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium" };
+              const styleProps = i + 1 === page ? activeProps : passiveProps;
+
+              return (
+                <button key={i} onClick={() => setPage(i + 1)} {...styleProps}> {i + 1} </button>
+              );
+            })}
+            <button className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <span className="sr-only">Next</span>
               <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
               </svg>
-            </a>
+            </button>
           </nav>
         </div>
       </div>
