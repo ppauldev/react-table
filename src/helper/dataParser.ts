7664a99db3b1e9fc87103bb5aeed1dc4e8c22857
Phillip Paul <phillippaul@gmx.de>
2022-08-31 17:24:00 +0200

    Add sorting layout, icons

--- src/helper/dataParser.ts
+++ src/helper/dataParser.ts
@@ -1,7 +1,7 @@
 import { TRowsData } from "../api/data";
 
 export const getColumnData = (data: TRowsData) => {
-  return data.length !== 0 ? Object.keys(data[0]) : []
+  return data.length !== 0 ? Object.keys(data[0]).map((column) => { return { label: column, isSortedAsc: false, isSortedDesc: false } }) : []
 };
 
 export const getRowData = (data: TRowsData, page: number, pageSize: number) => {
