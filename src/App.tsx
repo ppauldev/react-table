55e9c90dca594b5438a888bc66eabca0eb8d7d38
Phillip Paul <phillippaul@gmx.de>
2022-08-31 15:31:58 +0200

    Connect next, previous buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { TRowData } from "./api/data";
 import { TableContextProvider, useColumnContext, usePaginationContext, useRowContext } from "./contexts/TableContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { memo } from "react";
+import { NextIcon, PreviousIcon } from "./Icons";
 
 // Style via TailwindUI: https://tailwindui.com/components/application-ui/lists/tables
 
@@ -71,13 +73,13 @@ const TableRow = (rowProps: TRowData) => {
   );
 };
 
-const TableCell = ({ value }: { value: string }) => {
+const TableCell = memo(({ value }: { value: string }) => {
   return (
     <td className="px-6 py-4 text-center whitespace-nowrap text-sm">
       {value}
     </td>
   );
-};
+});
 
 const Pagination = () => {
   const { page, pageSize, setPage, count } = usePaginationContext();
@@ -98,11 +100,12 @@ const Pagination = () => {
         </div>
         <div>
           <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-            <button className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-              <span className="sr-only">Previous</span>
-              <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
-              </svg>
+            <button
+              onClick={() => setPage(page - 1)}
+              disabled={page === 1}
+              className={`relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 ${page !== 1 ? "hover:bg-gray-50" : "disabled:opacity-75 cursor-not-allowed"}`}
+            >
+              <PreviousIcon />
             </button>
             {[...Array((Math.ceil(count / pageSize) * pageSize) / pageSize)].map((_, i) => {
               const activeProps = { "aria-current": "page", "className": "z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium" };
@@ -113,11 +116,12 @@ const Pagination = () => {
                 <button key={i} onClick={() => setPage(i + 1)} {...styleProps}> {i + 1} </button>
               );
             })}
-            <button className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-              <span className="sr-only">Next</span>
-              <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-              </svg>
+            <button
+              onClick={() => setPage(page + 1)}
+              disabled={page + 1 > (Math.ceil(count / pageSize) * pageSize) / pageSize}
+              className={`relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 ${page + 1 <= (Math.ceil(count / pageSize) * pageSize) / pageSize ? "hover:bg-gray-50" : "disabled:opacity-75 cursor-not-allowed"}`}
+            >
+              <NextIcon />
             </button>
           </nav>
         </div>
