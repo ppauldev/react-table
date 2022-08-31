9122cd34cb76a72e1210687bf9aeecef34bfa126
Phillip Paul <phillippaul@gmx.de>
2022-08-31 10:44:08 +0200

    Add music data

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,52 +1,603 @@
 export type TColumnsData = string[];
 export type TRowData = {
-  "1": string;
-  "2": string;
-  "3": string;
+  "year": string;
+  "title": string;
+  "band": string;
+  "label": string;
 };
 export type TRowsData = TRowData[];
 
 const data: TRowsData = [
   {
-    "1": "A",
-    "2": "B",
-    "3": "C"
+    year: "1965",
+    title: "Like a Rolling Stone",
+    band: "Bob Dylan",
+    label: "Columbia"
   },
   {
-    "1": "D",
-    "2": "E",
-    "3": "F"
+    year: "1965",
+    title: "(I Can’t Get No) Satisfaction",
+    band: "The Rolling Stones",
+    label: "London"
   },
   {
-    "1": "G",
-    "2": "H",
-    "3": "I"
+    year: "1971",
+    title: "Imagine",
+    band: "John Lennon",
+    label: "Apple"
   },
   {
-    "1": "J",
-    "2": "K",
-    "3": "L"
+    year: "1971",
+    title: "What’s Going On",
+    band: "Marvin Gaye",
+    label: "Tamla"
   },
   {
-    "1": "M",
-    "2": "N",
-    "3": "O"
+    year: "1967",
+    title: "Respect",
+    band: "Aretha Franklin",
+    label: "Atlantic"
   },
   {
-    "1": "P",
-    "2": "Q",
-    "3": "R"
+    year: "1966",
+    title: "Good Vibrations",
+    band: "The Beach Boys",
+    label: "Capitol"
   },
   {
-    "1": "S",
-    "2": "T",
-    "3": "U"
+    year: "1958",
+    title: "Johnny B. Goode",
+    band: "Chuck Berry",
+    label: "Chess"
   },
   {
-    "1": "V",
-    "2": "W",
-    "3": "X"
+    year: "1968",
+    title: "Hey Jude",
+    band: "The Beatles",
+    label: "Apple"
   },
+  {
+    year: "1991",
+    title: "Smells Like Teen Spirit",
+    band: "Nirvana",
+    label: "DGC"
+  },
+  {
+    year: "1959",
+    title: "What’d I Say",
+    band: "Ray Charles",
+    label: "Atlantic"
+  },
+  {
+    year: "1965",
+    title: "My Generation",
+    band: "The Who",
+    label: "Decca"
+  },
+  {
+    year: "1964",
+    title: "A Change Is Gonna Come",
+    band: "Sam Cooke",
+    label: "RCA"
+  },
+  {
+    year: "1965",
+    title: "Yesterday",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1963",
+    title: "Blowin’ in the Wind",
+    band: "Bob Dylan",
+    label: "Columbia"
+  },
+  {
+    year: "1980",
+    title: "London Calling",
+    band: "The Clash",
+    label: "Epic"
+  },
+  {
+    year: "1963",
+    title: "I Want to Hold Your Hand",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1967",
+    title: "Purple Haze",
+    band: "The Jimi Hendrix Experience",
+    label: "Reprise"
+  },
+  {
+    year: "1955",
+    title: "Maybellene",
+    band: "Chuck Berry",
+    label: "Chess"
+  },
+  {
+    year: "1956",
+    title: "Hound Dog",
+    band: "Elvis Presley",
+    label: "RCA"
+  },
+  {
+    year: "1970",
+    title: "Let It Be",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1975",
+    title: "Born to Run",
+    band: "Bruce Springsteen",
+    label: "Columbia"
+  },
+  {
+    year: "1963",
+    title: "Be My Baby",
+    band: "The Ronettes",
+    label: "Philles"
+  },
+  {
+    year: "1965",
+    title: "In My Life",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1965",
+    title: "People Get Ready",
+    band: "The Impressions",
+    label: "ABC-Paramount"
+  },
+  {
+    year: "1966",
+    title: "God Only Knows",
+    band: "The Beach Boys",
+    label: "Capitol"
+  },
+  {
+    year: "1968",
+    title: "(Sittin’ On) The Dock of the Bay",
+    band: "Otis Redding",
+    label: "Volt"
+  },
+  {
+    year: "1970",
+    title: "Layla",
+    band: "Derek and the Dominos",
+    label: "Atco"
+  },
+  {
+    year: "1967",
+    title: "A Day in the Life",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1965",
+    title: "Help!",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1956",
+    title: "I Walk the Line",
+    band: "Johnny Cash",
+    label: "Sun"
+  },
+  {
+    year: "1971",
+    title: "Stairway to Heaven",
+    band: "Led Zeppelin",
+    label: "Atlantic"
+  },
+  {
+    year: "1968",
+    title: "Sympathy for the Devil",
+    band: "The Rolling Stones",
+    label: "London"
+  },
+  {
+    year: "1966",
+    title: "River Deep – Mountain High",
+    band: "Ike & Tina Turner",
+    label: "Philles"
+  },
+  {
+    year: "1964",
+    title: "You’ve Lost That Lovin’ Feelin’",
+    band: "The Righteous Brothers",
+    label: "Philles"
+  },
+  {
+    year: "1967",
+    title: "Light My Fire",
+    band: "The Doors",
+    label: "Elektra"
+  },
+  { year: "1991", title: "One", band: "U2", label: "Island" },
+  {
+    year: "1975",
+    title: "No Woman, No Cry",
+    band: "Bob Marley",
+    label: "Island"
+  },
+  {
+    year: "1969",
+    title: "Gimme Shelter",
+    band: "The Rolling Stones",
+    label: "London"
+  },
+  {
+    year: "1957",
+    title: "That’ll Be the Day",
+    band: "Buddy Holly and the Crickets",
+    label: "Brunswick"
+  },
+  {
+    year: "1964",
+    title: "Dancing in the Street",
+    band: "Martha Reeves and the Vandellas",
+    label: "Gordy"
+  },
+  {
+    year: "1968",
+    title: "The Weight",
+    band: "The Band",
+    label: "Capitol"
+  },
+  {
+    year: "1968",
+    title: "Waterloo Sunset",
+    band: "The Kinks",
+    label: "Reprise"
+  },
+  {
+    year: "1955",
+    title: "Tutti-Frutti",
+    band: "Little Richard",
+    label: "Specialty"
+  },
+  {
+    year: "1960",
+    title: "Georgia on My Mind",
+    band: "Ray Charles",
+    label: "ABC-Paramount"
+  },
+  {
+    year: "1956",
+    title: "Heartbreak Hotel",
+    band: "Elvis Presley",
+    label: "RCA"
+  },
+  {
+    year: "1977",
+    title: "Heroes",
+    band: "David Bowie",
+    label: "RCA"
+  },
+  {
+    year: "1968",
+    title: "All Along the Watchtower",
+    band: "The Jimi Hendrix Experience",
+    label: "Reprise"
+  },
+  {
+    year: "1970",
+    title: "Bridge over Troubled Water",
+    band: "Simon & Garfunkel",
+    label: "Columbia"
+  },
+  {
+    year: "1976",
+    title: "Hotel California",
+    band: "The Eagles",
+    label: "Asylum"
+  },
+  {
+    year: "1965",
+    title: "The Tracks of My Tears",
+    band: "Smokey Robinson and the Miracles",
+    label: "Tamla"
+  },
+  {
+    year: "1982",
+    title: "The Message",
+    band: "Grandmaster Flash & the Furious Five",
+    label: "Sugar Hill"
+  },
+  {
+    year: "1984",
+    title: "When Doves Cry",
+    band: "Prince",
+    label: "Warner Bros."
+  },
+  {
+    year: "1966",
+    title: "When a Man Loves a Woman",
+    band: "Percy Sledge",
+    label: "Atlantic"
+  },
+  {
+    year: "1963",
+    title: "Louie Louie",
+    band: "The Kingsmen",
+    label: "Jerden"
+  },
+  {
+    year: "1956",
+    title: "Long Tall Sally",
+    band: "Little Richard",
+    label: "Specialty"
+  },
+  {
+    year: "1977",
+    title: "Anarchy in the U. K.",
+    band: "Sex Pistols",
+    label: "Warner Bros."
+  },
+  {
+    year: "1967",
+    title: "A Whiter Shade of Pale",
+    band: "Procol Harum",
+    label: "A&M"
+  },
+  {
+    year: "1983",
+    title: "Billie Jean",
+    band: "Michael Jackson",
+    label: "Epic"
+  },
+  {
+    year: "1964",
+    title: "The Times They Are a-Changin’",
+    band: "Bob Dylan",
+    label: "Columbia"
+  },
+  {
+    year: "1971",
+    title: "Let’s Stay Together",
+    band: "Al Green",
+    label: "Hi"
+  },
+  {
+    year: "1957",
+    title: "Whole Lotta Shakin’ Goin’ On",
+    band: "Jerry Lee Lewis",
+    label: "Sun"
+  },
+  {
+    year: "1955",
+    title: "Bo Diddley",
+    band: "Bo Diddley",
+    label: "Checker"
+  },
+  {
+    year: "1967",
+    title: "For What It’s Worth",
+    band: "Buffalo Springfield",
+    label: "Atco"
+  },
+  {
+    year: "1963",
+    title: "She Loves You",
+    band: "The Beatles",
+    label: "Swan"
+  },
+  {
+    year: "1968",
+    title: "Sunshine of Your Love",
+    band: "Cream",
+    label: "Atco"
+  },
+  {
+    year: "1980",
+    title: "Redemption Song",
+    band: "Bob Marley and the Wailers",
+    label: "Island"
+  },
+  {
+    year: "1957",
+    title: "Jailhouse Rock",
+    band: "Elvis Presley",
+    label: "RCA"
+  },
+  {
+    year: "1975",
+    title: "Tangled Up in Blue",
+    band: "Bob Dylan",
+    label: "Columbia"
+  },
+  {
+    year: "1961",
+    title: "Crying",
+    band: "Roy Orbison",
+    label: "Monument"
+  },
+  {
+    year: "1964",
+    title: "Walk On By",
+    band: "Dionne Warwick",
+    label: "Scepter"
+  },
+  {
+    year: "1966",
+    title: "Papa’s Got a Brand New Bag",
+    band: "James Brown",
+    label: "King"
+  },
+  {
+    year: "1965",
+    title: "California Girls",
+    band: "The Beach Boys",
+    label: "Capitol"
+  },
+  {
+    year: "1972",
+    title: "Superstition",
+    band: "Stevie Wonder",
+    label: "Tamla"
+  },
+  {
+    year: "1958",
+    title: "Summertime Blues",
+    band: "Eddie Cochran",
+    label: "Liberty"
+  },
+  {
+    year: "1969",
+    title: "Whole Lotta Love",
+    band: "Led Zeppelin",
+    label: "Atlantic"
+  },
+  {
+    year: "1967",
+    title: "Strawberry Fields Forever",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1955",
+    title: "Mystery Train",
+    band: "Elvis Presley",
+    label: "Sun"
+  },
+  {
+    year: "1965",
+    title: "I Got You (I Feel Good)",
+    band: "James Brown",
+    label: "King"
+  },
+  {
+    year: "1965",
+    title: "Mr. Tambourine Man",
+    band: "The Byrds",
+    label: "Columbia"
+  },
+  {
+    year: "1964",
+    title: "You Really Got Me",
+    band: "The Kinks",
+    label: "Reprise"
+  },
+  {
+    year: "1968",
+    title: "I Heard It Through the Grapevine",
+    band: "Marvin Gaye",
+    label: "Tamla"
+  },
+  {
+    year: "1956",
+    title: "Blueberry Hill",
+    band: "Fats Domino",
+    label: "Imperial"
+  },
+  {
+    year: "1965",
+    title: "Norwegian Wood (This Bird Has Flown)",
+    band: "The Beatles",
+    label: "Capitol"
+  },
+  {
+    year: "1983",
+    title: "Every Breath You Take",
+    band: "The Police",
+    label: "A&M"
+  },
+  { year: "1961", title: "Crazy", band: "Patsy Cline", label: "Decca" },
+  {
+    year: "1975",
+    title: "Thunder Road",
+    band: "Bruce Springsteen",
+    label: "Columbia"
+  },
+  {
+    year: "1963",
+    title: "Ring of Fire",
+    band: "Johnny Cash",
+    label: "Columbia"
+  },
+  {
+    year: "1965",
+    title: "My Girl",
+    band: "The Temptations",
+    label: "Gordy"
+  },
+  {
+    year: "1965",
+    title: "California Dreamin’",
+    band: "The Mamas and the Papas",
+    label: "Dunhill"
+  },
+  {
+    year: "1956",
+    title: "In the Still of the Night",
+    band: "The Five Satins",
+    label: "Standord"
+  },
+  {
+    year: "1969",
+    title: "Suspicious Minds",
+    band: "Elvis Presley",
+    label: "RCA"
+  },
+  {
+    year: "1976",
+    title: "Blitzkrieg Bop",
+    band: "Ramones",
+    label: "Sire"
+  },
+  {
+    year: "1987",
+    title: "I Still Haven’t Found What I’m Looking For",
+    band: "U2",
+    label: "Island"
+  },
+  {
+    year: "1958",
+    title: "Good Golly, Miss Molly",
+    band: "Little Richard",
+    label: "Specialty"
+  },
+  {
+    year: "1956",
+    title: "Blue Suede Shoes",
+    band: "Carl Perkins",
+    label: "Sun"
+  },
+  {
+    year: "1957",
+    title: "Great Balls of Fire",
+    band: "Jerry Lee Lewis",
+    label: "Sun"
+  },
+  {
+    year: "1956",
+    title: "Roll Over Beethoven",
+    band: "Chuck Berry",
+    label: "Chess"
+  },
+  {
+    year: "1972",
+    title: "Love and Happiness",
+    band: "Al Green",
+    label: "Hi"
+  },
+  {
+    year: "1969",
+    title: "Fortunate Son",
+    band: "Creedence Clearwater Revival",
+    label: "Fantasy"
+  },
+  {
+    year: "2006",
+    title: "Crazy",
+    band: "Gnarls Barkley",
+    label: "Downtown"
+  }
 ];
 
 export default data;
